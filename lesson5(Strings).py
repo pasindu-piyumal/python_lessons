eaa623a90af4ba604ca5658182a40b7728bf4160
pasindu_piyumal <22cds0423@ms.sab.ac.lk>
2025-09-07 19:21:08 +0530

    more about strings in python

---
+++ lesson5(Strings).py
@@ -0,0 +1,17 @@
+course = "Python Programming"
+
+message = """ 
+Hi John,
+
+I am PAP.
+
+Blah blah blah
+"""
+
+print(len(course)) #Number of characters in a string
+print(course[0]) #print first character
+print(course[-1]) #print last character
+print(course[0:3]) #print first three characters
+print(course[0:])
+print(course[:3])
+print(course[:])
