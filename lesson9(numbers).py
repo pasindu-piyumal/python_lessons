9a931f1aaf6231612bf311e01ba83cd56bc11da0
pasindu_piyumal <22cds0423@ms.sab.ac.lk>
2025-09-08 10:32:46 +0530

    python numbers

---
+++ lesson9(numbers).py
@@ -0,0 +1,15 @@
+x = 1
+x = 1.1
+x = 1 + 2j 
+
+print(10 + 3)
+print(10 - 3)
+print(10 * 3)
+print(10 / 3) # floating point number
+print(10 // 3) # integer
+print(10 % 3) # remainder of the devision
+print(10 ** 3) # exponent = left to the power of right
+
+x = 10
+x = x + 3
+x += 3 # augmented assignment operator
