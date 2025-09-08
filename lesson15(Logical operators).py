10d531645268dbaa8606e2d538483fe854bd1b10
pasindu_piyumal <22cds0423@ms.sab.ac.lk>
2025-09-08 11:25:58 +0530

    logical operator in python

---
+++ lesson15(Logical operators).py
@@ -0,0 +1,27 @@
+# and 
+# or 
+# not
+
+high_income = False
+good_credit = True
+student = True
+
+if high_income and good_credit:
+    print("Eligible")
+else:
+    print("Not eligible")
+
+if high_income or good_credit:
+    print("Eligible")
+else:
+    print("Not eligible")
+
+if not student:
+    print("Eligible")
+else:
+    print("Not eligible")
+
+if (high_income or good_credit) and not student:
+    print("Eligible")
+else:
+    print("Not eligible")
