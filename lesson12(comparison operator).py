a54e2fbee8c53665835d78fe0bc3d53558406074
pasindu_piyumal <22cds0423@ms.sab.ac.lk>
2025-09-08 11:02:46 +0530

    checking comparison operators

---
+++ lesson12(comparison operator).py
@@ -0,0 +1,11 @@
+print(10 > 3)
+print(10 >= 3)
+print(10 < 20)
+print(10 <= 20)
+print(10 == 10)
+print(10 == "10")
+print(10 != "10")
+print("bag" > "apple")
+print("bag" == "BAG") # every charactor has a numeric representation
+print(ord("b")) # numeric representation of b
+print(ord("B")) # numeric representation of B
