50c24bcbe6623a5ba4f22516882b2a4020f184b2
pasindu_piyumal <22cds0423@ms.sab.ac.lk>
2025-09-08 10:15:24 +0530

    formatted strings in python

---
+++ lesson7(formatted strings).py
@@ -0,0 +1,13 @@
+first = "Pasindu"
+last = "Piyumal"
+full = first + " " + last
+print(full)
+
+full = f"{first} {last}"
+print(full)
+
+full = f"{len(first)} {last}"
+print(full)
+
+full = f"{len(first)} {last} {2 + 2}"
+print(full)
