2d5313782567fc01e930f51e6b596eb2ed323c73
pasindu_piyumal <22cds0423@ms.sab.ac.lk>
2025-09-11 08:29:01 +0530

    types of functions

---
+++ lesson27(types of functions).py
@@ -0,0 +1,12 @@
+def greet(name):
+    print(f"Hi {name}")
+
+
+# 1 - Perform a task
+# 2 - Return a value
+
+def get_greeting(name):
+    return f"Hi {name}"
+
+message = get_greeting("PAP")
+print(message)
